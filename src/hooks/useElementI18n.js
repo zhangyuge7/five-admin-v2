ac82c4f51c3d9f1bffb39acd8df3dc890d9f8ef1
zhangyuge <398698424@qq.com>
2023-12-30 23:35:01 +0800

    修改

--- src/hooks/useElementI18n.js
+++ src/hooks/useElementI18n.js
@@ -1,6 +1,6 @@
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import zhTw from 'element-plus/dist/locale/zh-tw.mjs'
-import en from 'element-plus/dist/locale/en.mjs'
+import zhCn from 'element-plus/dist/locale/zh-cn.js'
+import zhTw from 'element-plus/dist/locale/zh-tw.js'
+import en from 'element-plus/dist/locale/en.js'
 import { computed } from 'vue'
 import { useAppStore } from '@/stores/modules/app'
 
