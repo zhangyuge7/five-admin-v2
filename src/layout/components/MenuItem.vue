08070816828da36b75c66f29c48d026fc766bfa8
zhangyuge <398698424@qq.com>
2024-03-22 15:45:46 +0800

    子菜单标题文字超出省略

--- src/layout/components/MenuItem.vue
+++ src/layout/components/MenuItem.vue
@@ -29,7 +29,9 @@ function openLink(path) {
         <el-icon v-if="route.meta?.icon">
           <SvgIcon :name="route.meta.icon" />
         </el-icon>
-        <span v-if="route.meta?.title">{{ $t(route.meta.title) }}</span>
+        <span v-if="route.meta?.title" style="text-overflow: ellipsis;overflow: hidden;">
+          {{ $t(route.meta.title) }}
+        </span>
       </template>
       <MenuItem :routes="route.children" />
     </el-sub-menu>
@@ -38,7 +40,9 @@ function openLink(path) {
         <SvgIcon :name="route.meta.icon" />
       </el-icon>
       <template v-if="route.meta?.title" #title>
-        {{ $t(route.meta.title) }}
+        <span style="text-overflow: ellipsis;overflow: hidden;">
+          {{ $t(route.meta.title) }}
+        </span>
       </template>
     </el-menu-item>
   </template>
